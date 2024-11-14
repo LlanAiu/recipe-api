bf9fe2ca4a15e6ea2326907e1736498aebf1eb74
Alan Liu <liualan998@gmail.com>
2024-11-14 13:59:57 -0500

    small hover color change

--- frontend/app/find/ingredient-pill.tsx
+++ frontend/app/find/ingredient-pill.tsx
@@ -6,12 +6,14 @@ export default function IngredientPill({ingredient, index, onClick} : {
     index: number
     onClick: (ingredient: string) => void
 }) {
-    const colors = ["primary-bg", "secondary-bg", "tertiary-bg"];
+    const colors: string[] = ["primary-bg", "secondary-bg", "tertiary-bg"];
+    const hoverColors: string[] = ["hover:dark-primary-bg", "hover:dark-secondary-bg", "hover:dark-tertiary-bg"];
 
     const color: string = colors[index % colors.length];
+    const hoverColor: string = hoverColors[index % hoverColors.length];
 
     return (
-        <div className={`${color} hover:bg-blue-400 py-1 px-2 w-max h-max rounded-xl inline-block m-1`}>
+        <div className={`${color} ${hoverColor} py-1 px-2 w-max h-max rounded-xl inline-block m-1`}>
             <button type='button' className='w-max' onClick={() => onClick(ingredient)}>
                 <p className='w-max text-center text-sm'>{ingredient}</p>
             </button>
